dc14b364ae8da4256ae5346a5ae9cfa43158ed37
Dhavalkumar Ajana <ajana.dhaval.2@gmail.com>
2026-06-08 12:09:46 -0500

    Choose offsite grid

--- genie_samples/submit_wcsim_job.sh
+++ genie_samples/submit_wcsim_job.sh
@@ -13,8 +13,8 @@ echo ""
 echo "submitting job..."
 echo ""
 
-OUTPUT_FOLDER="${PNFS_PERSISTENT}/output/genie_wcsim_tank"
+OUTPUT_FOLDER="${PNFS_PERSISTENT}/output/genie_wcsim_tank/testv2/"
 mkdir -p $OUTPUT_FOLDER                                                 
 
 # wrapper script to submit your grid job
-jobsub_submit --memory=2000MB --expected-lifetime=4h -G annie --disk=5GB --resource-provides=usage_model=OFFSITE --blacklist=Omaha,Swan,Wisconsin,SU-ITS,RAL -f ${INPUT_PATH}/WCSim.tar.gz -f ${INPUT_PATH}/wcsim_container.sh -f ${DIRT}/annie_tank_flux.${RUN}.root -f ${GENIE}/gntp.${RUN}.ghep.root -d OUTPUT $OUTPUT_FOLDER file://${INPUT_PATH}/run_job.sh ${RUN}
+jobsub_submit --memory=2000MB --expected-lifetime=4h -G annie --disk=5GB --resource-provides=usage_model=ONSITE --blacklist=Omaha,Swan,Wisconsin,SU-ITS,RAL -f ${INPUT_PATH}/WCSim.tar.gz -f ${INPUT_PATH}/wcsim_container.sh -f ${DIRT}/annie_tank_flux.${RUN}.root -f ${GENIE}/gntp.${RUN}.ghep.root -d OUTPUT $OUTPUT_FOLDER file://${INPUT_PATH}/run_job.sh ${RUN}
